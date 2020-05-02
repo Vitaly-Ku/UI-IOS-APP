5f9d6ea8c47cb3c345157d5e2fe8f7a2640d2fd6
Vitaly Kulagin <vitaly.kulagin.85@bk.ru>
2020-05-02 14:32:01 +0300

    singleton Session

---
+++ Lesson1/Views/AlamofireServices.swift
@@ -0,0 +1,17 @@
+//
+//  AlamofireServices.swift
+//  Lesson1
+//
+//  Created by Vit K on 28.04.2020.
+//  Copyright © 2020 Vitaly Kulagin. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AlamofireServices {
+    public func sendRequest() {
+//        Alamofire.request()
+        
+    }
+}
---
+++ Lesson1/patterns/Singletons.swift
@@ -0,0 +1,19 @@
+//
+//  Singletons.swift
+//  Lesson1
+//
+//  Created by Vit K on 26.04.2020.
+//  Copyright © 2020 Vitaly Kulagin. All rights reserved.
+//
+
+import Foundation
+
+class Session {
+    private init() {}
+
+    public static let shared = Session()
+
+    var token: String = ""
+    var userId: Int = 0
+    var baseUrl: String = ""
+}
